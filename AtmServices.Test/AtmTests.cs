27e35211a1880a6f9f91355ed69945ec7b4910fa
Ethan Buckner <122373712+Smokinghalo8@users.noreply.github.com>
2025-11-17 19:42:38 +0000

    100% Succeed real

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -19,7 +19,7 @@ public class AtmTests
  
 
     [Fact]
-public void Test_Withdraw()
+    public void Test_Withdraw()
     {
 
         var result = testAtm.withdraw(25);
@@ -27,6 +27,31 @@ public void Test_Withdraw()
         Assert.True(result);
 
         Assert.Equal(75, testAtm.getBalance());
+    }
+
+
+    [Fact]
+    public void Test_Insufficient_Funds()
+    {
+        var result = testAtm.withdraw(500);
+        Assert.False(result);
+        Assert.Equal(100, testAtm.getBalance());
+    }
+
+    [Fact]
+    public void TestDepsotite()
+    {
+        var result = testAtm.deposit(500);
+        Assert.True(result);
+        Assert.Equal(600, testAtm.getBalance());
+    }
+
+    [Fact]
+    public void TestDepoLessThanMin()
+    {
+        var result = testAtm.deposit(-100);
+        Assert.False(result);
+        Assert.Equal(100, testAtm.getBalance());
     }
 
 }
